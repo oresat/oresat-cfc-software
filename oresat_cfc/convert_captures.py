770b68c0cdabce7369675d108c790de0db90b53c
Oliver Rew <oliver.rew@rigado.com>
2021-08-26 06:51:54 +0000

    add most of capture loop

---
+++ oresat_cfc/convert_captures.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import gzip
+import numpy as np
+import cv2
+
+cols = 1280
+rows = 1024
+
+capt_dir = sys.argv[1]
+
+# read all the files in the capture directory
+capts = os.listdir(capt_dir)
+
+for capt in capts:
+    # only try to convert gzip'd files
+    if not capt.endswith(".gz"):
+        continue
+
+    # create the full file path
+    path = capt_dir + "/" + capt
+    print(path)
+
+    # open the gzip open
+    with gzip.open(path, 'rb') as f:
+        # read the decompressed bytes
+        buf = f.read()
+
+        # read image bytes into ndarray
+        img = np.frombuffer(buf, dtype=np.uint16).reshape(rows, cols)
+
+        # try to encode the image as png
+        ok, encoded = cv2.imencode(".png", img, params=[cv2.CV_16U])
+        if not(ok):
+            raise Exception("error encoding image")
+
+        # write the file as a png
+        png_path = path.replace(".gz", ".png")
+        with open(png_path, 'wb') as png:
+            png.write(encoded.tobytes())
+
--- oresat_cfc/main.py
+++ oresat_cfc/main.py
@@ -1,16 +1,185 @@
+#!/usr/bin/python3
 """Main for the OreSat CFC daemon."""
 
-from time import sleep
+import time
+import gzip
+import numpy as np
+import sys
+from pydbus import SystemBus
+import os
+import io
+import logging
+from gi.repository import GLib
+import signal
+import threading
+import cv2
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# TODO remove this when on octavo
+try:
+    from cfc_tec import ctrl as tec
+except:
+    pass
+from pirt1280 import pirt1280
+
+DBUS_INTERFACE_NAME = "org.OreSat.CFC"
+
+tec_setpoint = -10
+
+# constants
+cols = 1280
+rows = 1024
+pixel_bytes = cols * rows * 2
+prucam_path = "/dev/prucam"
+
+capture_dir = "/home/rew/cfc_captures"
+
+# instantiate the PIRT once here. 
+# TODO there is nothing wrong with doing this multiple times elsewhere
+# TODO remove 'try:' when on octavo
+try:
+    pirt = pirt1280()
+except:
+    pass
+
+def run_cfc_test_forever():
+    try: # TODO remove on octavo
+        pirt.enable()
+    except:
+        pass
+
+    # wait a sec
+    time.sleep(1)
+    
+    try: # TODO remove on octavo
+        tec.start(tec_setpoint)
+    except:
+        pass
+
+    while True:
+        log.info("capturing...")
+        
+        get_frame_with_integration(0.01)
+
+        time.sleep(1)
+
+def get_frame_with_integration(intr):
+    try: # TODO remove on octavo
+        pirt.set_integration(intr)
+    except:
+        pass
+    
+    # wait a moment for settings to apply
+    time.sleep(0.05)
+    
+    # open the prucam char device
+    fd = os.open(prucam_path, os.O_RDWR)
+    fio = io.FileIO(fd, closefd = False)
+
+    # allocate buffer to read frame into
+    imgbuf = bytearray(pixel_bytes)
+
+    # read from prucam into buffer
+    fio.readinto(imgbuf)
+
+    # close the char device
+    os.close(fd)
+
+    # TODO make filename with time, integration time, and ....?
+    
+    temp = -12.1
+    try:
+        temp = tec.get_temp()
+    except:
+        pass
+
+    # replace decimal point with 'p' to not mess up extension
+    temp_str = str(temp).replace(".","p")
+    
+    filename = "capt_" + str(intr) + "_" + temp_str + "C_" + str(int(time.time())) + ".gz"
+   
+    # TODO test image
+    # TODO compress image?
+
+    # write raw buffer out to file gzip'd
+    with gzip.open(capture_dir + "/" + filename, 'wb') as out:
+        out.write(imgbuf)
+
+
+
+def kill_dbus(*args):
+    GLib.MainLoop().quit()
+    sys.exit(-1)
 
 
 def main():
     """The main for the oresat CFC daemon"""
 
     try:
-        # TODO replace
-        while True:
-            sleep(1)
-    except KeyboardInterrupt:
-        return 0
+        # run test in separate thread
+        threading.Thread(target=run_cfc_test_forever).start()
 
+        # set up dbus wrapper
+        cfc = DBusServer()
+        bus = SystemBus()
+        bus.publish(DBUS_INTERFACE_NAME, cfc)
+
+        loop = GLib.MainLoop()
+
+        # TODO remove these
+        signal.signal(signal.SIGINT, kill_dbus)
+        signal.signal(signal.SIGTERM, kill_dbus)
+        signal.signal(signal.SIGQUIT, kill_dbus)
+
+        try:
+            loop.run()
+        except KeyboardInterrupt:
+            print("KEYBOARD")
+            loop.quit()
+        except Exception as e:
+            print("EXCEPTION")
+            loop.quit()
+
+    except Exception as e:
+        print("exception:", e)
+
+        return 0
     return 1
+
+# TODO
+# - endpoint for temperature
+# - endpoint for number of pics taken
+# - endpoint for diskspace
+# - disable when cannot stay at temp?
+# - todo compress image
+
+
+class DBusServer():
+
+    # D-Bus interface(s) definition
+    dbus = """
+    <node>
+        <interface name="org.OreSat.CFC">
+            <property name="CFCState" type="s" access="read" />
+            <property name="CaptureCount" type="u" access="read" />
+        </interface>
+    </node>
+    """
+    
+    @property
+    def CFCState(self) -> str:
+        return "GOOD STATE"
+
+    @property
+    def CaptureCount(self) -> int:
+        log.info("get capture count")
+        
+        # return the number of files in the capture directory
+        files = os.listdir(capture_dir)
+        return len(files)
+
+# TODO remove this
+if __name__ == "__main__":
+    main()
