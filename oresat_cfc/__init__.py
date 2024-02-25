d5d16c770728067d1ab7906a12178d0ee0df315e
Ryan Medick <rmedick@protonmail.com>
2024-02-25 10:06:44 -0800

    use setuptools-scm for package version

--- oresat_cfc/__init__.py
+++ oresat_cfc/__init__.py
@@ -1,3 +1,6 @@
 """OreSat CFC"""
 
-__version__ = "1.0.0"
+try:
+    from ._version import version as __version__  # type: ignore
+except ImportError:
+    __version__ = "0.0.0"  # package is not installed
