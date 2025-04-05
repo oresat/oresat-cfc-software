13de768c2fc11ed9677f408bae671296aa8cdbff
Ryan Medick <rmedick@protonmail.com>
2025-04-05 06:46:51 -0700

    fix capture download via ui and rework of gen script

--- gen.py
+++ gen.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
-import os
-from oresat_configs import write_canopend_od, gen_od, OdConfig
+import shutil
+from argparse import ArgumentParser
 
-GEN_DIR = "oresat_cfc/gen"
-os.makedirs(GEN_DIR, exist_ok=True)
+from oresat_configs import gen_canopend_files, gen_dbc_node
 
-init_file = os.path.join(GEN_DIR, "__init__.py")
-if not os.path.isfile(init_file):
-    open(init_file, "w").close()
+OD_CONFIG_PATH = "od.yaml"
+GEN_DIR_PATH = "oresat_cfc/gen"
 
-od_config = OdConfig.from_yaml("od.yaml")
-od = gen_od([od_config])
-write_canopend_od("cfc", od, GEN_DIR)
+parser = ArgumentParser()
+parser.add_argument("gen", nargs="?", choices=["code", "dbc", "clean"], default="code")
+args = parser.parse_args()
+
+if args.gen == "code":
+    gen_canopend_files(OD_CONFIG_PATH, GEN_DIR_PATH)
+elif args.gen == "dbc":
+    gen_dbc_node(OD_CONFIG_PATH)
+elif args.gen == "clean":
+    shutil.rmtree(GEN_DIR_PATH, ignore_errors=True)
